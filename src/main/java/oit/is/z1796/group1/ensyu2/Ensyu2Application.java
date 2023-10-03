5d7d27e620bc5060a58ef0ecc3c91a87e52e59fa
kiyo4649 <e1b21107@oit.ac.jp>
2023-10-03 14:46:38 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1796/group1/ensyu2/Ensyu2Application.java
@@ -0,0 +1,13 @@
+package oit.is.z1796.group1.ensyu2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ensyu2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ensyu2Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1796/group1/ensyu2/Ensyu2ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1796.group1.ensyu2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ensyu2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
